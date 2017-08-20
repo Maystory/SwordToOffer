1b75a29b9f4f32414df3d93311b28a5dc0e1896a
meifan <meifan_mvc@foxmail.com>
2017-08-20 15:08:36 +0800

    添加11

--- src/cn/swun/swordToOffer/Demo11.java
+++ src/cn/swun/swordToOffer/Demo11.java
@@ -4,16 +4,28 @@
 package cn.swun.swordToOffer;
 
 /**
- * @author 梅凡
- *
+ * 
+ * .实现函数double Power(double base,int exponent)，
+ * 求base的exponent次方。不得使用库函数，
+ * 同时不需要考虑大数问题。
  */
-public class Demo11 {
-
-	/**
-	 * @param 
-	 */
-	public static void main(String[] args) {
+public class Demo11{
+	public static double Power(double base,int exponent){
+		if(exponent==0){
+			return 1;
+		}
+		if(exponent==1){
+			return base;
+		}
+		double result = Power(base,exponent>>1);
+		result*=result;
+		if((exponent&0x1)==1){
+			result*=base;
+		}
+		return result;
+	}
+ public static void main(String[] args){
+	 System.out.println(Power(2.0,56));
 
 	}
-
 }
